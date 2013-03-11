ab2dd610f559dc150fc0f7f3433239778c104cde
Vincent van Haaff <vincent@vanhaaff.com>
2013-03-11 09:30:57 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Younagrarians::Application.config.secret_token = 'baa297844e30b2a7e66d4b4a7a6cdda47ec63c7065c12fe616b8b43acd4fd110a8a903172e7d06a770d4af0986759d8c331162169f2bce9ab2de80aa6e303134'
